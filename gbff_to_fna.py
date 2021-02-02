2d9db1712dab6fd5c2246371db620005afbb40fe
flayner2 <flayner5@gmail.com>
2021-02-01 21:16:13 -0300

    Updated formatting

--- gbff_to_fna.py
+++ gbff_to_fna.py
@@ -7,13 +7,13 @@ Example run inside LGM: `python3 gbff_to_fna.py`
 """
 
 # Change this to the input path containing your files
-input_path = '../data/genomes/'
+input_path = "../data/genomes/"
 # Chage this to the desired output path. It doesn't need to already be created
-output_path = '../data/fna/'
+output_path = "../data/fna/"
 
 for filename in os.listdir(input_path):
-    if filename.endswith('.gbff'):
-        in_name = f'{input_path}{filename}'
+    if filename.endswith(".gbff"):
+        in_name = f"{input_path}{filename}"
         outname = f'{output_path}{filename.replace(".gbff", ".fna")}'
-        print(f'Converting the file {str(filename)}')
-        SeqIO.convert(in_name, 'genbank', outname, 'fasta')
+        print(f"Converting the file {str(filename)}")
+        SeqIO.convert(in_name, "genbank", outname, "fasta")
