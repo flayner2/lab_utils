a35fbacdc9859c209d44c45bcf727c787444a027
flayner2 <flayner5@gmail.com>
2021-02-02 14:13:13 -0300

    created a test for the size filtering function

--- trim_ests.py
+++ trim_ests.py
@@ -15,4 +15,19 @@ def filter_by_size(seq: SeqRecord, n: int) -> bool:
     return len(seq) > n
 
 
-for seq in SeqIO.parse("./Polistes_canadensis_ests.fasta", "fasta"):
+def main():
+    for seq in SeqIO.parse("./Polistes_canadensis_ests.fasta", "fasta"):
+        pass
+
+
+def test_filter_by_size():
+    valid_seq = SeqRecord(seq="ACTGCTG", id="valid")
+    invalid_seq = SeqRecord(seq="ACTG", id="invalid")
+    min_size = 5
+
+    assert filter_by_size(valid_seq, min_size) == True
+    assert filter_by_size(invalid_seq, min_size) == False
+
+
+if __name__ == "__main__":
+    main()
