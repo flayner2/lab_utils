1875c78438767d8821d5b553d7b67f3e9c54b80f
flayner2 <flayner5@gmail.com>
2021-03-23 16:03:46 -0300

    Need to fix the build_output function

---
+++ bed_overlaps_stats.py
@@ -0,0 +1,112 @@
+from Bio import SeqIO
+
+import sys
+import csv
+
+
+def read_bed(path: str) -> list[dict]:
+    """
+    A BED file is nothing more than a tsv file so this simple function just reads a
+    tsv file with a specific conformation, that is, the output of bedtools intersect
+    with the -wo flag.
+
+    Arguments:
+        path (str): a path to a BED file.
+
+    Returns:
+        list[dict]: a list of dictionaries containing information about each contig
+        on a BED file.
+    """
+    result = []
+
+    with open(path, "r") as bed_file:
+        csv_reader = csv.reader(bed_file, delimiter="\t")
+
+        for row in csv_reader:
+            each_row = {}
+            each_row["scaffold"] = row[0]
+            each_row["contig"] = row[3]
+            each_row["locus"] = row[7]
+            each_row["op_len"] = int(row[-1])
+
+            result.append(each_row)
+
+    return result
+
+
+def read_fasta(path: str) -> list[dict]:
+    """
+    Uses Biopython to read a FASTA file containing contigs that correspond to those
+    from a BED file.
+
+    Arguments:
+        path: path to a FASTA file.
+
+    Returns:
+        list[dict]: a list of dictionaries containing the id of the contig and its
+        length.
+    """
+    results = []
+
+    for record in SeqIO.parse(path, "fasta"):
+        each_record = {}
+        each_record["contig"] = record.id
+        each_record["len"] = len(record.seq)
+
+        results.append(each_record)
+
+    return results
+
+
+def build_output(
+    bed: list[dict], fasta: list[dict], outname: str = "output.tsv"
+) -> None:
+    """
+    Write the information about contig coverages to a file.
+
+    Arguments:
+        bed (list[dict]): a list of dictionaries containing information about a set of
+        BED records.
+        fasta (list[dict]): a list of dictionaries containing information about a set of
+        FASTA records.
+        outname (str): a name for the output file. Defaults to "output.tsv".
+    """
+    with open(outname, "w") as outfile:
+        outfile.write("scaffold\tcontig\tlocus\top_len\tcontig_len\tcoverage\n")
+
+        for contig in fasta:
+            for b_record in bed:
+                if contig["contig"] != b_record["contig"]:
+                    break
+
+                coverage = b_record["op_len"] / contig["len"]
+                outfile.write(
+                    (
+                        f"{b_record['scaffold']}\t{b_record['contig']}\t"
+                        f"{b_record['locus']}\t{b_record['op_len']}\t{contig['len']}\t"
+                        f"{coverage}\n"
+                    )
+                )
+
+
+def main() -> None:
+    try:
+        bed_records = read_bed(sys.argv[1])
+        fasta_records = read_fasta(sys.argv[2])
+        if len(sys.argv) > 3:
+            build_output(bed_records, fasta_records, sys.argv[3])
+        else:
+            build_output(bed_records, fasta_records)
+    except IndexError:
+        sys.exit(
+            (
+                "Didn't provide valid paths to at least one of the expected files or"
+                " the ouptut file name."
+            )
+        )
+    except Exception as e:
+        sys.exit(f"Error occurred while trying to run: {e}")
+
+
+if __name__ == "__main__":
+    main()
