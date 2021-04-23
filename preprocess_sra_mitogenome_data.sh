44db6243295c50801638fa0b28efe96155cb4b70
flayner2 <flayner5@gmail.com>
2021-04-23 13:49:10 -0300

    Refactored the raw read counts, included bwa into the routine

--- preprocess_sra_mitogenome_data.sh
+++ preprocess_sra_mitogenome_data.sh
@@ -2,14 +2,20 @@
 
 # Provide two arguments when running:
 input=$1 # The input file with the ids
-outdir=$2 # The directory where to put the fastq files
+ref=$2 # The reference genome
+fastq_dir=$3 # The directory where to put the fastq files
+outdir=$4 # The directory where to put the sam file
 
 ids=$(cat $input)
 
 for id in $ids 
-do 
+do
         prefetch $id
-        fastq-dump --split-files --outdir $outdir $id
-        printf "Reads for $id:\n" && grep -c ">" ${outdir}/${id}*
-        rm -f ${outdir}/${id}*
+        fastq-dump --split-files --fastq_dir $fastq_dir $id
+        raw_reads_1=$(grep -c -h ">" ${fastq_dir}/${id}_1*)
+        raw_reads_2=$(grep -c -h ">" ${fastq_dir}/${id}_2*)
+        printf "# of raw reads:\n1: $raw_reads_1\n2: $raw_reads_2"
+        bwa mem $ref ${fastq_dir}/${id}_1* ${fastq_dir}/${id}_2* > ${outdir}/{$id}_bwa_out.sam
+        #rm -f ${fastq_dir}/${id}*
+        #rm -f $SRA_CACHE/*
 done
