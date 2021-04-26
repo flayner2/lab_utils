fbf1042b0919d5689bd20e9555fb564826ec780b
flayner2 <flayner5@gmail.com>
2021-04-26 15:58:36 -0300

    Included the samtools conversion into the pipeline

--- preprocess_sra_mitogenome_data.sh
+++ preprocess_sra_mitogenome_data.sh
@@ -5,6 +5,7 @@ input=$1 # The input file with the ids
 ref=$2 # The reference genome
 fastq_dir=$3 # The directory where to put the fastq files
 outdir=$4 # The directory where to put the sam file
+threads=${5:-1}
 
 ids=$(cat $input)
 
@@ -17,12 +18,12 @@ fi
 for id in $ids 
 do
         prefetch $id
-        #fastq-dump --split-files --outdir $fastq_dir $id
-        fasterq-dump $id --split-files --outdir $fastq_dir -e 4
+        fasterq-dump $id --split-files --outdir $fastq_dir -e $threads
         rm -f $SRA_CACHE/* # Export this variable with the path to your prefetch downloads folder
         raw_reads_1=$(grep -c -h "${id}" ${fastq_dir}/${id}_1*)
         raw_reads_2=$(grep -c -h "${id}" ${fastq_dir}/${id}_2*)
         bwa mem $ref ${fastq_dir}/${id}_1* ${fastq_dir}/${id}_2* > ${outdir}/${id}_bwa_out.sam
+        samtools view -b -o ${id}_bwa_out.bam -@ $((threads - 1)) ${outdir}/${id}_bwa_out.sam 
         printf "# of raw reads:\n1: ${raw_reads_1}\n2: ${raw_reads_2}\n"
         #rm -f ${fastq_dir}/${id}*
 done
