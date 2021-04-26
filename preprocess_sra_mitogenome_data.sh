6f2197234c2f8d73356e8bf4dea852be9ffb34ff
flayner2 <flayner5@gmail.com>
2021-04-26 20:08:13 -0300

    Added samtools sort and index to the pipeline

--- preprocess_sra_mitogenome_data.sh
+++ preprocess_sra_mitogenome_data.sh
@@ -4,8 +4,8 @@
 input=$1 # The input file with the ids
 ref=$2 # The reference genome
 fastq_dir=$3 # The directory where to put the fastq files
-outdir=$4 # The directory where to put the sam file
-threads=${5:-1}
+outdir=$4 # The directory where to put the result files
+threads=${5:-1} # The number of threads to run certain programs
 
 ids=$(cat $input)
 
@@ -23,7 +23,11 @@ do
         raw_reads_1=$(grep -c -h "${id}" ${fastq_dir}/${id}_1*)
         raw_reads_2=$(grep -c -h "${id}" ${fastq_dir}/${id}_2*)
         bwa mem $ref ${fastq_dir}/${id}_1* ${fastq_dir}/${id}_2* > ${outdir}/${id}_bwa_out.sam
-        samtools view -b -o ${id}_bwa_out.bam -@ $((threads - 1)) ${outdir}/${id}_bwa_out.sam 
+        samtools view -b -o ${outdir}/${id}_bwa_out.bam -@ $((threads - 1)) ${outdir}/${id}_bwa_out.sam
+        rm -f ${outdir}/${id}_bwa_out.sam
+        samtools sort -o ${outdir}/${id}_bwa_out.sorted.bam -@ $threads ${outdir}/${id}_bwa_out.bam
+        rm -f ${outdir}/${id}_bwa_out.bam
+        samtools index -b -@ $threads ${outdir}/${id}_bwa_out.sorted.bam
         printf "# of raw reads:\n1: ${raw_reads_1}\n2: ${raw_reads_2}\n"
         #rm -f ${fastq_dir}/${id}*
 done
