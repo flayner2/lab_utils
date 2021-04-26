61845c026e11a08d7d7064a920f3b80dd3f051de
flayner2 <flayner5@gmail.com>
2021-04-26 11:50:55 -0300

    Worked for the first step throughout

--- preprocess_sra_mitogenome_data.sh
+++ preprocess_sra_mitogenome_data.sh
@@ -10,18 +10,19 @@ ids=$(cat $input)
 
 # Check if the indexed database files don't already exist
 # before creating them with `bwa index`
-if [! -f "${ref}.*"]; then 
+if [ ! -f "${ref}.*" ]; then 
         bwa index $ref
 fi
 
 for id in $ids 
 do
-        #prefetch $id
+        prefetch $id
         #fastq-dump --split-files --outdir $fastq_dir $id
-        #rm -f $SRA_CACHE/* # Export this variable with the path to your prefetch downloads folder
-        #raw_reads_1=$(grep -c -h "${id}" ${fastq_dir}/${id}_1*)
-        #raw_reads_2=$(grep -c -h "${id}" ${fastq_dir}/${id}_2*)
-        #printf "# of raw reads:\n1: ${raw_reads_1}\n2: ${raw_reads_2}\n"
+        fasterq-dump $id --split-files --outdir $fastq_dir -e 4
+        rm -f $SRA_CACHE/* # Export this variable with the path to your prefetch downloads folder
+        raw_reads_1=$(grep -c -h "${id}" ${fastq_dir}/${id}_1*)
+        raw_reads_2=$(grep -c -h "${id}" ${fastq_dir}/${id}_2*)
         bwa mem $ref ${fastq_dir}/${id}_1* ${fastq_dir}/${id}_2* > ${outdir}/${id}_bwa_out.sam
+        printf "# of raw reads:\n1: ${raw_reads_1}\n2: ${raw_reads_2}\n"
         #rm -f ${fastq_dir}/${id}*
 done
