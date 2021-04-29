8d3a5890d45f639e8349fe289aa494b60777e676
flayner2 <flayner5@gmail.com>
2021-04-28 23:36:11 -0300

    Fixed some of the samtools steps

--- preprocess_sra_mitogenome_data.sh
+++ preprocess_sra_mitogenome_data.sh
@@ -31,26 +31,25 @@ do
         bwa mem -t $threads -T $mapq_qual $ref ${fastq_dir}/${id}_1* ${fastq_dir}/${id}_2* > ${outdir}/${id}_bwa_out.sam
         rm -f ${fastq_dir}/${id}*
 
-        # Use samtools to convert SAM to BAM and remove the SAM file
-        samtools view -b -q $mapq_qual -o ${outdir}/${id}_bwa_out.bam -@ $threads ${outdir}/${id}_bwa_out.sam
+        # Sort the SAM file, removing the unsorted SAM
+        samtools sort -n -o ${outdir}/${id}_bwa_out.sorted.bam -@ $threads ${outdir}/${id}_bwa_out.sam
         rm -f ${outdir}/${id}_bwa_out.sam
-        
-        # Sort and index the BAM files, removing the unsorted BAM
-        samtools sort -n -o ${outdir}/${id}_bwa_out.sorted.bam -@ $threads ${outdir}/${id}_bwa_out.bam
-        rm -f ${outdir}/${id}_bwa_out.bam
-        samtools index -b -@ $threads ${outdir}/${id}_bwa_out.sorted.bam
+
+        # Use samtools to convert SAM to BAM and remove the SAM file
+        samtools view -F 0x04 -f 0x2 -q $mapq_qual -o ${outdir}/${id}_bwa_out.sorted.filtered.bam -@ $threads -b ${outdir}/${id}_bwa_out.sorted.bam
+        rm -f ${outdir}/${id}_bwa_out.sorted.bam
         
         # Convert BAM to fastq, removing the BAM and BAI files
         # This conversion only outputs paired reads. Singletons and non-paired reads are discarded
-        samtools fastq -1 ${outdir}/${id}_enriched_1.fq -2 ${outdir}/${id}_enriched_2.fq -0 /dev/null -s /dev/null -n ${outdir}/${id}_bwa_out.bam.bai
+        samtools fastq -1 ${outdir}/${id}_enriched_1.fq.gz -2 ${outdir}/${id}_enriched_2.fq.gz -0 /dev/null -s /dev/null -n ${outdir}/${id}_bwa_out.sorted.filtered.bam
         rm -f ${outdir}/${id}_bwa_out.*
 
         # Calculate the needed stats and print the report out
         enriched_reads_1=$(grep -c -h "${id}" ${outdir}/${id}_enriched_1*)
         enriched_reads_2=$(grep -c -h "${id}" ${outdir}/${id}_enriched_2*)
-        ratio_1=$(echo "scale=4 ; $enriched_reads_1 / $raw_reads_1" | bc)
-        ratio_2=$(echo "scale=4 ; $enriched_reads_2 / $raw_reads_2" | bc)
-        printf "# of raw reads:\n1: ${raw_reads_1}\n2: ${raw_reads_2}\n"
-        printf "# of enriched reads:\n1: ${enriched_reads_1}\n2: ${enriched_reads_2}\n"
-        printf "Proportion of mitochondrial reads on the original SRA dataset:\n1: ${ratio_1}\n2: ${ratio_2}\n"
+        ratio_1=$(echo "scale=6 ; $enriched_reads_1 / $raw_reads_1" | bc)
+        ratio_2=$(echo "scale=6 ; $enriched_reads_2 / $raw_reads_2" | bc)
+        printf "# of raw reads:\n1: ${raw_reads_1}\n2: ${raw_reads_2}\n" >> ${outdir}/${id}_report.txt
+        printf "# of enriched reads:\n1: ${enriched_reads_1}\n2: ${enriched_reads_2}\n" >> ${outdir}/${id}_report.txt
+        printf "Proportion of enriched reads on the original SRA dataset:\n1: ${ratio_1}\n2: ${ratio_2}\n" >> ${outdir}/${id}_report.txt
 done
