0120eb9a610a1139d71ec49e37851f9eac6f5823
flayner2 <flayner5@gmail.com>
2021-05-10 15:36:28 -0300

    Added some new steps to the pipeline

--- get_genes_via_blast.sh
+++ get_genes_via_blast.sh
@@ -1,9 +1,13 @@
 dbs_list=$(ls /home/maycon/Documents/LAB/eusociality/local_data/genes/raw_seqs)
 genomes_path="/mnt/2A2A43C42A438C2F/lab_data/genomes/2021-04-19/new/to_annotate"
+blast_res_path="/mnt/2A2A43C42A438C2F/lab_data/genes_blast/2021-05-07"
+bed_path="/mnt/2A2A43C42A438C2F/lab_data/bed_results/genes_bed/2021-05-10"
 
 for genome_file in `ls $genomes_path/*.fna`; do
         for db in $dbs_list; do
                 blastn -query $genome_file -db $db -outfmt 6 -out ${genome_file}_${db}.blastn -num_threads 4 -evalue 1e-6
         done
+
+        mv ${genomes_path}/*.blastn $blast_res_path
 done
 
