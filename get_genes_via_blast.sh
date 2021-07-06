896c5fd65e0441e6c626177abd8381aa9a0e9460
flayner2 <flayner5@gmail.com>
2021-07-06 19:32:57 -0300

    Fixed a problem with file names

--- get_genes_via_blast.sh
+++ get_genes_via_blast.sh
@@ -4,8 +4,10 @@ queries_list=$(ls /home/maycon/Documents/LAB/eusociality/local_data/genes/raw_se
 subjects_list=$(ls /mnt/2A2A43C42A438C2F/lab_data/longest_cds/2021-07-06/originals/*.fasta)
 blast_res_path="/mnt/2A2A43C42A438C2F/lab_data/genes_blast/2021-07-06"
 
-for query in $queries_list; do
-        for subject in $subjects_list; do
-                blastn -query $query -subject $subject -outfmt 6 -out $blast_res_path/${subject}_blast_${query}_results.tsv
+for query_path in $queries_list; do
+        query=${query_path##*/}
+        for subject_path in $subjects_list; do
+                subject=${subject_path##*/}
+                blastn -query $query_path -subject $subject_path -outfmt 6 -out $blast_res_path/${subject}_blast_${query}_results.ts
         done
 done
