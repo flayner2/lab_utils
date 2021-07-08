2e75b8bff9789259232fd9dce8f76000f63a8c6d
flayner2 <flayner5@gmail.com>
2021-07-08 20:08:46 -0300

    Changed the parameters for another run

--- get_genes_via_blast.sh
+++ get_genes_via_blast.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-queries_list=$(ls /home/maycon/Documents/LAB/eusociality/local_data/genes/raw_seqs/2021-07-01/partial/*.fas)
-subjects_list=$(ls /mnt/2A2A43C42A438C2F/lab_data/longest_cds/2021-07-06/originals/*.fasta)
-blast_res_path="/mnt/2A2A43C42A438C2F/lab_data/genes_blast/2021-07-06"
+queries_list=$(ls /mnt/2A2A43C42A438C2F/lab_data/genes_results/2021-07-08/originals_complete_cds/*.fas)
+subjects_list=$(ls /mnt/2A2A43C42A438C2F/lab_data/longest_cds/2021-07-06/new_annotated/*.longest)
+blast_res_path="/mnt/2A2A43C42A438C2F/lab_data/genes_blast/2021-07-08"
 
 for query_path in $queries_list; do
         query=${query_path##*/}
